5fa70d8c09150c537652fe3bfa15897bcac523f4
merong245 <37497183+merong245@users.noreply.github.com>
2021-11-21 21:37:35 +0900

    로그인 구현

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var bodyParser=require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var register=require('./routes/router');
+var login=require('./routes/router');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register',register);
+app.use('/login',login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,34 +3,86 @@ const pool=require('./config')
 
 const router = require('.')
 
+// 회원가입
+router.get('/register',(req,res)=>{
+    console.log('회원가입 페이지');
+    res.render('register');
+});
 
 router.post('/register',(req,res,next)=>{
     const join=[req.body.name,req.body.registration_number,req.body.age,req.body.sex,req.body.phone_number]
     const login=[req.body.id,req.body.passwd,req.body.registration_number]
     const location=[req.body.province,req.body.city,req.body.district]
-    if(req.body.passwd===req.body.checkpasswd)
-    {
+
+    if(req.body.passwd===req.body.checkpasswd) {
         pool.getConnection(function(err,connection){
+
 
             connection.query('INSERT INTO user(`name`,`registration_number`,`age`,`sex`,`phone_number`) VALUES (?,?,?,?,?)',join,(err,row)=>{
                 if(err) console.log(err)
             })
-    
+
             connection.query('INSERT INTO login(`id`,`passwd`,`fk_registration_number`) VALUES (?,?,?)',login,(err,row)=>{
                 if(err) console.log(err)
             })
+
             connection.query('INSERT INTO location(`province`,`city`,`district`) VALUES (?,?,?)',location,(err,row)=>{
                 if(err) console.log(err)
             })
-                
+
+            res.redirect('/login')
+            res.send('<script>alert("회원가입 완료");</script>')
+            connection.release();
             
         })
     }
     else
+    {
         console.log('비밀번호 불일치');
+    }
     
     
     res.end()
 })
+
+// 로그인
+router.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+router.post('/login',(req,res)=> {
+    const id = req.body.id;
+    const passwd = req.body.passwd;
+
+
+    pool.getConnection(function (err, connection) {
+
+        var sqlForSelectList = "SELECT * FROM login WHERE id=" + "'" + req.body.id + "'";
+
+        connection.query(sqlForSelectList,(err, row) => {
+            if (err) console.log(err)
+            if(!row.length){
+                console.log('로그인 실패');
+                console.log('아이디와 비밀번호를 확인하세요.');
+                res.render('login');
+            }
+            else if (id == row[0].id && passwd == row[0].passwd) {
+                console.log('로그인 성공');
+                res.redirect('/');
+            }
+            else{
+                console.log('로그인 실패');
+                console.log('아이디와 비밀번호를 확인하세요.');
+                res.render('login');
+                //res.send('<script>alert("아이디와 비밀번호를 확인하세요.");</script>');
+            }
+            console.log(JSON.stringify(row));
+        })
+
+        connection.release();
+    })
+
+});
+
 
 module.exports = router;
