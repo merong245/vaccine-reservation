8aef2380a091b901fe69c9d6e0b5e52e5407fc31
shin-hd <shinhd1998@gmail.com>
2021-11-28 22:05:50 +0900

    SideNavigator 구현

---
+++ frontend/src/components/common/SideNavigator.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { Navigation } from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import { RiHomeLine } from 'react-icons/ri';
+import { MdOutlineLocalHospital } from 'react-icons/md';
+import { FaRegCalendarTimes } from 'react-icons/fa';
+import { BsTable } from 'react-icons/bs';
+
+const SideNavigation = ({ history }) => {
+  const items = [
+    {
+      title: '홈',
+      itemId: '/',
+      elemBefore: () => {
+        return <RiHomeLine />;
+      },
+    },
+    {
+      title: '병원별 잔여백신 현황',
+      itemId: '/login',
+      elemBefore: () => {
+        return <MdOutlineLocalHospital />;
+      },
+    },
+    {
+      title: '예약가능 의료기관 조회',
+      itemId: '/register',
+      elemBefore: () => {
+        return <FaRegCalendarTimes />;
+      },
+    },
+    {
+      title: '접종결과',
+      itemId: '/home',
+      elemBefore: () => {
+        return <BsTable />;
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Navigation
+        onSelect={({ itemId }) => {
+          history.push(itemId);
+        }}
+        items={items}
+      />
+    </>
+  );
+};
+
+export default withRouter(SideNavigation);
